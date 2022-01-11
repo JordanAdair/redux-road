3c7c3292b98d77f77bc569eb0c7920534e6c0692
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-01-11 15:40:36 -0500

    Added "gather" reducer case

--- reduxRoad.js
+++ reduxRoad.js
@@ -4,3 +4,19 @@ const initialWagonState = {
 	distance: 0,
 	days: 0,
 };
+
+// A reducer that will manage the state of the game
+const gameReducer = (state = initialWagonState, action) => {
+	switch (action.type) {
+		// A player may gather supplies, which takes them a day and doesn’t cover any distance.
+		case "gather":
+			return {
+				...state,
+				supplies: state.supplies + 15,
+				days: state.days + 1,
+			};
+
+		default:
+			return state;
+	}
+};
